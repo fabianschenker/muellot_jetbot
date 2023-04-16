fe1d8f5107208544d963516a309f3f2f9812eb63
fabianschenker <93080522+fabianschenker@users.noreply.github.com>
2023-04-16 12:21:23 +0200

    motor control test

--- jetbot/__init__.py
+++
@@ -1,6 +0,0 @@
-from .camera import Camera
-from .heartbeat import Heartbeat
-from .motors import Motors
-from .robot2 import Robot2
-from .image import bgr8_to_jpeg
-from .object_detection import ObjectDetector
