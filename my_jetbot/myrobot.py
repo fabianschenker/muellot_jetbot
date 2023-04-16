e22308403edfb1b2a209f0018f9c2a51745f51ea
fabianschenker <93080522+fabianschenker@users.noreply.github.com>
2023-04-16 09:25:36 +0200

    motor control test

--- my_jetbot/myrobot.py
+++ my_jetbot/myrobot.py
@@ -1,8 +1,27 @@
 import time
 import traitlets
 from traitlets.config.configurable import SingletonConfigurable
+import smbus2
 from .mymotors import MyMotors
 
+bus = smbus2.SMBus(1)
+
+# This is the address we setup in the Arduino Program
+address = 0x04
+
+def writeData(value):
+    byteValue = StringToBytes(value)
+    bus.write_i2c_block_data(address,0x00,byteValue) #first byte is 0=command byte.. just is.
+    return -1
+
+def StringToBytes(val):
+        retVal = []
+        for c in val:
+                retVal.append(ord(c))
+        return retVal
+
+def _map(x, in_min, in_max, out_min, out_max):
+    return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
 
 class MyRobot(MyMotors):
     i2c_address = traitlets.Integer(default_value=0x04)
@@ -11,14 +30,25 @@ class MyRobot(MyMotors):
     steering_channel = traitlets.Integer(default_value=0)
     throttle_gain = traitlets.Float(default_value=0.8)
     throttle_channel = traitlets.Integer(default_value=1)
+    steering_f = traitlets.Float()
+    throttle_f = traitlets.Float()
 
     def __init__(self, *args, **kwargs):
         super(MyRobot, self).__init__(*args, **kwargs)
 
     @traitlets.observe('steering')
     def _on_steering(self, change):
-        self.steering_motor = change['new'] * self.steering_gain + self.steering_offset
+        steering_f = change['new'] * self.steering_gain + self.steering_offset
+        message = "ss"
+        val = _map(steering_f, -1.0, 1.0, 50, 130)
+        var = message + str(val)
+        writeData(var)
+
 
     @traitlets.observe('throttle')
     def _on_throttle(self, change):
-        self.throttle_motor = change['new'] * self.throttle_gain
+        throttle_f = change['new'] * self.throttle_gain
+        message = "ms"
+        val = _map(throttle_f, -1.0, 1.0, -255, 255)
+        var = message + str(val)
+        writeData(var)
